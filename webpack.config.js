6f66c9689e15186e6e29d4b1ec01036ae44c151d
Wjpayne <payne.j.will@gmail.com>
2026-01-13 14:06:53 -0600

    run webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -7,7 +8,6 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: '/Restaurant-Page-Odin/',
     clean: true,
   },
   devtool: "eval-source-map",
